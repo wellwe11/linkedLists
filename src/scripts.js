79145edf809ab748e7db06419e8f832f842cc116
Robin Ryan <robin1ryan@hotmail.com>
2024-11-04 10:07:14 +0100

    finished project. Will update extra credit in future

--- src/scripts.js
+++ src/scripts.js
@@ -2,13 +2,14 @@
 export default function LinkedList() {
   // LinkedList class / factory, which will represent the full list.
 
-  // Add unique nodes to LinkedList; contain list of all nodes
+  // add unique nodes to LinkedList; contain list of all nodes
   const Node = (val) => ({ value: val || null, nextNode: null });
 
   let first = null;
   let last = null;
 
   const append = (val) => {
+    // append(value) adds a new node containing value to the end of the list
     const currNode = Node(val);
     if (!first) {
       first = currNode;
@@ -17,39 +18,84 @@ export default function LinkedList() {
       last.nextNode = currNode;
       last = currNode;
     }
-    // append(value) adds a new node containing value to the end of the list
   };
 
-  const prepend = () => {
+  const prepend = (val) => {
     // prepend(value) adds a new node containing value to the start of the list
+    const currNode = Node(val);
+    currNode.nextNode = first;
+    first = currNode;
   };
 
   const size = () => {
     // size returns the total number of nodes in the list
+    let num = 0;
+    let current = first;
+    while (current) {
+      current = current.nextNode;
+      num++;
+    }
+
+    return num;
   };
 
   const head = () => {
     // head returns the first node in the list
+    return first;
   };
 
   const tail = () => {
     // tail returns the last node in the list
+    return last;
   };
 
-  const at = () => {
+  const at = (index) => {
     // at(index) returns the node at the given index
+    let num = 0;
+    let current = first;
+    while (current) {
+      if (index === num) break;
+      current = current.nextNode;
+      num++;
+    }
+
+    return current;
   };
 
   const pop = () => {
     // pop removes the last element from the list
+    let current = first;
+    while (current) {
+      if (current.nextNode === last) {
+        last = current;
+        last.nextNode = null;
+        break;
+      }
+      current = current.nextNode;
+    }
+    return current;
   };
 
-  const contains = () => {
+  const contains = (val) => {
     // contains(value) returns true if the passed in value is in the list and otherwise returns false.
+    let current = first;
+    while (current) {
+      if (current.value === val) return true;
+      current = current.nextNode;
+    }
+    return false;
   };
 
-  const find = () => {
+  const find = (val) => {
     // find(value) returns the index of the node containing value, or null if not found.
+    let current = first;
+    let index = 0;
+    while (current) {
+      if (current.value === val) return index;
+      index++;
+      current = current.nextNode;
+    }
+    return null;
   };
 
   const toString = () => {
@@ -67,6 +113,8 @@ export default function LinkedList() {
 
   const insertAt = () => {
     // insertAt(value, index) that inserts a new node with the provided value at the given index.
+    let current = first;
+    let next = current.nextNode;
   };
 
   const removeAt = () => {
@@ -75,7 +123,17 @@ export default function LinkedList() {
 
   return {
     append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
     toString,
+    insertAt,
+    removeAt,
   };
 }
 
@@ -84,8 +142,16 @@ const someThing = LinkedList();
 someThing.append("dog");
 someThing.append("horse");
 someThing.append("cat");
+someThing.prepend("donkey");
+someThing.prepend("horseTwo");
+someThing.prepend("horseThree");
+someThing.pop();
+someThing.pop();
 
 console.log(someThing.toString());
+console.log(someThing.contains("donkey"));
+console.log(someThing.find("dog"));
+console.log(someThing.find("donkey"));
 
 // when creating a new LinkedList, and appending something, it will append a "new Node object"
 
